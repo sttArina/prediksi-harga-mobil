4373a95be6a3a9899dca449e6b63d889be4d94bf
sttArina <120069969+sttArina@users.noreply.github.com>
2022-12-08 08:25:06 +0700

    Add files via upload

---
+++ stream-carprice.py
@@ -0,0 +1,19 @@
+import pickle
+import streamlit as st
+
+car_model = pickle.load(open('carprice_model.sav', 'rb'))
+
+st.title('Prediksi Harga Mobil Data Mining')
+
+year = st.text_input('Masukan Tahun Mobil')
+mileage = st.text_input('Masukan Mileage')
+tax = st.text_input('Masukan Pajak Mobil')
+mpg = st.text_input('Masukan rata rata kebutuhan bahan bakar')
+engineSize = st.text_input('Masukan ukuran mesin')
+
+car_predict = ''
+
+if st.button('Prediksi'):
+    car_predict = car_model.predict(
+        [[year, mileage, tax, mpg, engineSize]])
+    st.write('Predicted Car Price in EUR : ', car_predict)
